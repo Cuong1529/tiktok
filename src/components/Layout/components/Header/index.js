78f6e5beee43750c188074db5bf4022c4ba46937
Brucelee <conthuong151295@gmail.com>
2022-06-22 00:05:27 +0700

    build UI header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,24 +1,38 @@
-import styles from './Header.module.scss'
-import classNames from 'classnames/bind';
+import styles from "./Header.module.scss";
+import classNames from "classnames/bind";
+import images from "../../../../assets/images";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return (
-        <header className={cx('wrapper')}>
-            <div className={cx('inner')}>
-                <div className={cx('logo')}>
-                    <h1>Tiktok</h1>
-                </div>
-                <div className={cx('search')}>
-                    <input placeholder='Search accounts and videos'/>
-                </div>
-                <div className={cx('upload')}>
-                    <button>Upload</button>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx("search")}>
+          <input placeholder="Search accounts and videos" />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon icon={faSpinner} className={cx("loading")} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("upload")}>
+          <button>Upload</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
